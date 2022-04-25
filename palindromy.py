1de1bf0636a576fb7c806d8d5f33c4266caff618
Marcin Uciński <mucinski@poczta.onet.pl>
2022-04-25 09:53:12 +0200

    small final polishing

---
+++ palindromy.py
@@ -0,0 +1,9 @@
+k = str(input('podaj proszę słowo: '))
+n = 0
+f = -1
+if k[n] == k[f]:
+    n += 1
+    f -=1
+    print('jupii!! to jest palindrom')
+else:
+    print('eee..., normalne słowo, nuda')
